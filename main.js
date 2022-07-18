395f4ffec4d353fb7b7d8e570c08c01d84b0b943
Nerwen <jenny2209@gmx.de>
2022-07-18 14:05:44 +0200

    implement renderGameboard function

--- main.js
+++ main.js
@@ -5,13 +5,28 @@ const Player = (token) => {
 };
 
 // create two players
-const player1 = player("X");
-const player2 = player("O");
+const player1 = Player("X");
+const player2 = Player("O");
 
-// Module pattern for gameboard and displayController
+// Module pattern for gameboard and Controller
 
 const gameboard = (() => {
-  const _board = [];
+  const board = ["O", "", "X", "", "", "O", "", "", ""];
+
+  const renderGameboard = () => {
+    //take array, run through it with loop, if it has O or X place it in the field, with correlates with the index, should be in html the data index
+    for (let i = 0; i < board.length; i++) {
+      // get html div that correlates with the index
+      const boardCell = document.querySelector(`[data-index="${i}"]`);
+      if (board[i] == "O") {
+        boardCell.textContent = "O";
+      } else if (board[i] == "X") {
+        boardCell.textContent = "X";
+      }
+    }
+  };
+
+  return { renderGameboard };
 })();
 
-const displayController = (() => {})();
+const gameController = (() => {})();
